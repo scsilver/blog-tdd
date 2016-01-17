47650c018779db15e7b3cd310d95837ccb2d5bd2
Scsilver <scsilver.umd@gmail.com>
2016-01-16 20:31:32 -0700

    Add create index and post_params to posts controller
    Add alerts and redirects for post#create
    Add @posts assigns Post.all
    Add params for post_params requiring post and permiting title and content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Your post has been created."
+      redirect_to @post
+
+    else
+      flash[:alert] = "Your post is invalid!"
+      render :new
+    end
+  end
+  def index
+    @posts = Post.all
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
